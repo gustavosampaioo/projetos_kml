413fbeed88411426b8b653b448c90cb11350174e
gustavosampaioo <eng.gustavosampaio@gmail.com>
2025-03-07 14:04:09 -0300

    Update projetos_kml.py

--- projetos_kml.py
+++ projetos_kml.py
@@ -331,11 +331,12 @@ if uploaded_file is not None:
                 tooltip=f"{nome_folder} - {nome_placemark}"
             ).add_to(mapa)
     
+    # Adiciona marcadores das cidades com tamanho menor e exibe o nome diretamente no mapa
     for nome, coord in cidades_coords:
         folium.Marker(
             location=coord,
-            icon=folium.Icon(icon="home", color="green"),
-            popup=nome
+            icon=folium.Icon(icon="home", color="green", icon_size=(10, 10)),  # Ajusta o tamanho do ícone
+            tooltip=nome  # Exibe o nome do placemark diretamente no mapa
         ).add_to(mapa)
     
     folium_static(mapa)
